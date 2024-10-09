6ff09d48065fb1a174ade6728a1bac4d8142ec83
user.name : aaradhayasingh811 <user.email : aaradhayasingh811@gmail.com>
2024-10-10 01:23:55 +0530

    added content

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -14,36 +14,31 @@ const Event = () => {
   
   <div className="maincontainer rounded">
     <div className="back">
-      <h2>Copywriting</h2>
+      <h2>Bytegram</h2>
       <p>
-        Introduction to Copywriting’ workshop focuses on the theory and
-        processes of professional copywriting as applied to persuasion,
-        reasoning, and rhetoric. This workshop is best-suited to learning how to
-        write and think about consumer-driven functions.
+      Bytegram, organized by the University Innovation Cell, comprises three engaging rounds. The first round, "Tick the Bit," is an online quiz hosted on Unstop, featuring 40 questions to be answered in 20 minutes. The second round, "Flash the Brain," is an offline quiz focusing on rapid one-liner responses. The final buzzer round " Guess the Logic" tests participants' ability to guess logic-based answers swiftly, making for an exciting and intellectually stimulating experience.
       </p>
     </div>
     <div className="front mx-auto">
       <div className="image">
         <img classname="img mx-auto" src={bytegram} />
-        <h2>Copywriting</h2>
+        <h2>BYTEGRAM</h2>
       </div>
     </div>
   </div>
   {/* 2 */}
   <div className="maincontainer rounded">
     <div className="back">
-      <h2>Copywriting</h2>
+      <h2>Malviyan Prodigy</h2>
       <p>
-        Introduction to Copywriting’ workshop focuses on the theory and
-        processes of professional copywriting as applied to persuasion,
-        reasoning, and rhetoric. This workshop is best-suited to learning how to
-        write and think about consumer-driven functions.
+      Malaviyan Prodigy is the event which brings forward the best of the Malaviyan talent who are currently placed in top positions across their domains and are the alumnus of this college and give a chance to the freshmen to interact and learn from their experiences
+      These sessions aim to motivate and guide students as they begin their college journey, offering them the unique opportunity to engage directly with alumni and gain valuable direction and knowledge.
       </p>
     </div>
     <div className="front  mx-auto">
       <div className="image">
         <img classname="img" src={malviyan} />
-        <h2>Copywriting</h2>
+        <h2>MALVIYAN PRODIGY</h2>
       </div>
     </div>
   </div>
@@ -51,16 +46,14 @@ const Event = () => {
     <div className="back">
       <h2>Innowizion</h2>
       <p>
-        Introduction to Content Marketing workshop focuses on building content
-        frameworks that are designed for and directed at communication
-        engagement. This interdisciplinary workshop is best-suited to learning
-        visual and written communication strategies.
+      Innowizion is a dynamic fest organized by the University Innovation Cell, featuring a diverse array of engaging and entertaining events. It fosters creativity and collaboration, offering participants opportunities to learn, innovate, and showcase their talents in solving real world problems  while promoting knowledge exchange and networking among students.
+      INNOWIZION first held in 2016, has grown with time to became the largest and one of the most awaited fests due to its uniqueness and exotic events, as well as due to the enthusiasm and creativity that is witnessed every year. 
       </p>
     </div>
     <div className="front  mx-auto">
       <div className="image">
         <img classname="img" src={inno} />
-        <h2>Content Marketing</h2>
+        <h2>INNOWIZION</h2>
       </div>
     </div>
   </div>
@@ -68,17 +61,13 @@ const Event = () => {
     <div className="back">
       <h2>Induction</h2>
       <p>
-        Introduction to Web-Writing workshop focuses on building creative and
-        systemic digital content through online user experiences that benefit
-        people and robots. This workshop is best-suited to creating content for
-        digital platforms and devices — websites, mobile, game consoles, and
-        virtual reality engines.
+      The objective of this process is to identify passionate, creative, and dedicated individuals capable of contributing to the innovation and entrepreneurial ecosystem within the university. The induction process is divided into two rounds, with a detailed focus on form evaluation and personal interviews.
       </p>
     </div>
     <div className="front">
       <div className="image  mx-auto">
         <img classname="img" src={induction} />
-        <h2>Web Writing</h2>
+        <h2>INDUCTION</h2>
       </div>
     </div>
   </div>
